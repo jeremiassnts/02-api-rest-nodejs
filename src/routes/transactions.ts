b4f2f057ebe7b6818ed42254b2ac9b3189f0470b
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-06-04 16:35:24 -0300

    feat: cookies fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,32 @@
 import { FastifyInstance } from "fastify"
 import { z } from 'zod'
 import { knex } from "../database"
-import crypto from 'crypto'
+import crypto, { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select()
+
+        return {
+            transactions
+        }
+    })
+    app.get('/:id', async (request) => {
+        const getTransactionsParamsSchema = z.object({
+            id: z.string().uuid()
+        })
+        const { id } = getTransactionsParamsSchema.parse(request.params)
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+    app.get('/summary', async () => {
+        const summary = await knex('transactions')
+            .sum('amount', { as: 'amount' })
+            .first()
+
+        return { summary }
+    })
     app.post('/', async (req, res) => {
         const createTransactionBodySchema = z.object({
             title: z.string(),
@@ -12,10 +35,21 @@ export async function transactionsRoutes(app: FastifyInstance) {
         })
 
         const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+        let sessionId = req.cookies.sessionId
+        if (!sessionId) {
+            sessionId = randomUUID()
+            res.cookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 1000 * 60 * 60 * 24 * 7
+            })
+        }
+
         await knex('transactions').insert({
             id: crypto.randomUUID(),
             title,
-            amount: type == 'credit' ? amount : amount * -1
+            amount: type == 'credit' ? amount : amount * -1,
+            session_id: sessionId
         })
 
         return res.status(201).send()
