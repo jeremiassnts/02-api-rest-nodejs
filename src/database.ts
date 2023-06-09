cd36cc41cebb0d884949dc4a9a3edb0111bc3ac7
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-06-09 20:20:25 -0300

    fix: connection and client

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,10 @@ import path from 'path'
 import { env } from './env'
 
 export const config: Knex.Config = {
-    client: env.DATABASE_CLIENT === 'sqlite' ? {
-        filename: env.DATABASE_URL
-    } : env.DATABASE_URL,
-    connection: {
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_CLIENT == 'sqlite' ? {
         filename: path.join(__dirname, env.DATABASE_URL)
-    },
+    } : env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
